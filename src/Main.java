e26a840e4dd2962209e85fd0a8d6420072992154
rahulg3520 <rahul@nestgroup.net>
2022-11-21 12:44:52 +0530

    largest

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+
+    public static int largest(int a,int b,int c)
+    {
+        if(a>=b && a>=c)
+            return a;
+        else if (b>=a && b>=c)
+            return b;
+        else
+            return c;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("1:Largest of 3 numbers");
